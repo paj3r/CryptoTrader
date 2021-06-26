996866b937cb964b7ccb4bb694a3b55e8785cab5
paj3r <ivo.pajer@gmail.com>
2021-06-26 12:08:51 +0200

    env update, dodan RSI, ker NVTS ni primeren za altcoine

--- main.py
+++ main.py
@@ -2,27 +2,53 @@ import math
 from datetime import date
 from dateutil.relativedelta import relativedelta
 import pandas_datareader.data as pdr
+import pandas_ta as pta
 import numpy as np
 import pandas as pd
 import yfinance as yf
 
 yf.pdr_override()
-coins = ['XRP-USD', 'BTC-USD', 'ETH-USD', 'BNB-USD', 'ADA-USD', 'DOT1-USD', 'BCH-USD', 'UNI3-USD', 'LTC-USD', 'XLM-USD']
+coins = ['XRP-USD', 'BTC-USD', 'ETH-USD', 'BNB-USD', 'ADA-USD', 'DOT1-USD', 'BCH-USD', 'UNI3-USD', 'LTC-USD',
+         'XLM-USD']
 # teže koliko bomo upoštevali kater indikator v optimizaciji format [SHARPE(3m), SHARPE(6m), SORTINO(3m)]
-weights_ratios = [0.3, 0.3, 0.4]
+weights_ratios = [0.5, 0.5, 0]
 datum = date.today()
 datum_3m = datum + relativedelta(months=-3)
 datum_6m = datum + relativedelta(months=-6)
 
 
-def get_data(names, startdate, enddate):
+def get_closing(names, startdate, enddate):
     df = pdr.get_data_yahoo(names, startdate, enddate)['Adj Close']
     df.sort_index(inplace=True)
     return df
 
 
+def get_volume(names, startdate, enddate):
+    df = pdr.get_data_yahoo(names, startdate, enddate)['Volume']
+    df.sort_index(inplace=True)
+    return df
+
+
+def get_marketCap(names, startdate, enddate):
+    df = pdr.get_quote_yahoo(names, startdate, enddate)['marketCap']
+    df.sort_index(inplace=True)
+    return df
+
+
+def get_4h_data(names, startdate, enddate):
+    df = pdr.get_data_yahoo(names, startdate, enddate, interval='1h')['Adj Close']
+    df.sort_index(inplace=True)
+    df = df.iloc[::4, :]
+    return df
+
+
 def replace_negatives(table):
     table[table < 0] = math.nan
+    return table
+
+
+def replace_zeroes(table):
+    table[table < 0.10] = 0.0
     return table
 
 
@@ -89,7 +115,11 @@ def optimisation(coin_names, mean_return_3m, mean_return_6m, mean_return_poz_3m,
     print("Ratio_pre: "+str(calculate_ratios(mean_return_3m, mean_return_6m, mean_return_poz_3m, cov_matrix_3m,
                                        cov_matrix_6m, cov_matrix_poz_3m, old_weights)))
     old_weights = np.round(old_weights / np.linalg.norm(old_weights, 1.0), 2)
-    print("Ratio: "+str(calculate_ratios(mean_return_3m, mean_return_6m, mean_return_poz_3m, cov_matrix_3m,
+    print("Ratio after rounding: "+str(calculate_ratios(mean_return_3m, mean_return_6m, mean_return_poz_3m, cov_matrix_3m,
+                                       cov_matrix_6m, cov_matrix_poz_3m, old_weights)))
+    old_weights = replace_zeroes(old_weights)
+    old_weights = np.round(old_weights / np.linalg.norm(old_weights, 1.0), 2)
+    print("Ratio after >10% rule: "+str(calculate_ratios(mean_return_3m, mean_return_6m, mean_return_poz_3m, cov_matrix_3m,
                                        cov_matrix_6m, cov_matrix_poz_3m, old_weights)))
     return old_weights
 
@@ -109,9 +139,21 @@ def calculate_ratios(mean_return_3m, mean_return_6m, mean_return_poz_3m, cov_mat
     rez = sharpe_3m * weights_ratios[0] + sharpe_6m * weights_ratios[1] + sortino_3m * weights_ratios[2]
     return rez
 
+def calculate_RSI(prices, dolz):
+    table = pd.DataFrame(list(zip(coins, np.zeros(len(coins)))), columns=['Coins', 'RSI'])
+    for index in range(len(coins)):
+        table.loc[index, 'RSI'] = pta.rsi(prices[coins[index]], length=dolz)[-1]
+    return table
 
-prices_3m = get_data(coins, datum_3m, datum)
-prices_6m = get_data(coins, datum_6m, datum)
+
+prices_3m = get_closing(coins, datum_3m, datum)
+prices_6m = get_closing(coins, datum_6m, datum)
+prices_4h = get_4h_data(coins, datum_3m, datum)
+#volums_3m = get_volume(coins, datum_3m, datum).mean()
+#print(volums_3m)
+#mkcap = get_marketCap(coins, datum_3m, datum)
+#nvts = mkcap/volums_3m
+#rint(nvts)
 day_returns_3m = prices_3m.pct_change()
 day_returns_6m = prices_6m.pct_change()
 day_returns_poz_3m = replace_negatives(prices_3m.pct_change())
@@ -126,3 +168,4 @@ sortino_3m_cov = day_returns_poz_3m.cov()
 results = optimisation(coins, mean_sharpe_3m, mean_sharpe_6m, mean_sortino_3m, sharpe_3m_cov, sharpe_6m_cov,
                        sortino_3m_cov)
 print(pd.DataFrame(list(zip(coins, results*100)), columns=['Coins', 'Weights (%)']))
+print(calculate_RSI(prices_4h, 14))
